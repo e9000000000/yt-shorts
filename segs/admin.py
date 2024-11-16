b77fecce7c68bc9e82517de4eb7dda4787744a13
e <skipper224483@gmail.com>
2024-11-16 06:16:00 +0300

    +remove_error admin action

--- segs/admin.py
+++ segs/admin.py
@@ -4,12 +4,19 @@ from . import models
 
 admin.site.register(models.Channel)
 
+
 @admin.action()
 def make_for_next_uploading(modeladmin, request, queryset):
     queryset.update(error=None, uploaded_at=None)
+
+
+@admin.action()
+def remove_error(modeladmin, request, queryset):
+    queryset.update(error=None)
+
 
 @admin.register(models.Clip)
 class ClipAdmin(admin.ModelAdmin):
     list_display = ("title", "is_uploaded", "is_error", "channel", "video")
     list_filter = ("channel", "uploaded_at", "error")
-    actions = (make_for_next_uploading,)
+    actions = (make_for_next_uploading, remove_error)
